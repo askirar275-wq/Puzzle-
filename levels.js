1854a00438486607bc93a127302c38809969d38b
Candy match <askirar275@gmail.com>
2026-07-21 14:07:48 +0530

    Create levels.js

---
+++ levels.js
@@ -0,0 +1,26 @@
+const LEVELS = [
+
+{
+    nodes:[
+        {x:0.2,y:0.2,color:"#ff3b30"},
+        {x:0.8,y:0.8,color:"#ff3b30"},
+
+        {x:0.8,y:0.2,color:"#007aff"},
+        {x:0.2,y:0.8,color:"#007aff"}
+    ]
+},
+
+{
+    nodes:[
+        {x:0.2,y:0.2,color:"#ff3b30"},
+        {x:0.8,y:0.8,color:"#ff3b30"},
+
+        {x:0.8,y:0.2,color:"#34c759"},
+        {x:0.2,y:0.8,color:"#34c759"},
+
+        {x:0.5,y:0.2,color:"#007aff"},
+        {x:0.5,y:0.8,color:"#007aff"}
+    ]
+}
+
+];
